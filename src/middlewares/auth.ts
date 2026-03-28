56bc50ae2b36325854689371b6ee1fe852ee5d06
Nurul Alam <nurulalam0404@gmail.com>
2026-03-28 19:40:00 +0600

    fixed cookie issue

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,59 +1,47 @@
 import { NextFunction, Request, Response } from "express";
-import { auth as betterAuth } from '../lib/auth'
+import { auth as betterAuth } from '../lib/auth';
+import { fromNodeHeaders } from "better-auth/node";
 
 export enum UserRole {
-    STUDENT = "STUDENT",
-    TUTOR = "TUTOR",
-    ADMIN = "ADMIN",
+  STUDENT = "STUDENT",
+  TUTOR = "TUTOR",
+  ADMIN = "ADMIN",
 }
 
-
 const auth = (...roles: UserRole[]) => {
-    return async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const sessionToken =
-                req.cookies?.["__Secure-session-token"] ||
-                req.cookies?.["session-token"];
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const session = await betterAuth.api.getSession({
+        headers: fromNodeHeaders(req.headers),
+      });
 
-            const headers = new Headers();
-            
-            if (sessionToken) {
-                headers.set("cookie", `session-token=${sessionToken}`);
-            } else {
-                Object.entries(req.headers).forEach(([key, value]) => {
-                    if (value) headers.set(key, Array.isArray(value) ? value.join(",") : value);
-                });
-            }
+      if (!session) {
+        return res.status(401).json({
+          success: false,
+          message: "You are not authorized!",
+        });
+      }
 
-            const session = await betterAuth.api.getSession({ headers });
+      req.user = {
+        id: session.user.id,
+        email: session.user.email,
+        name: session.user.name,
+        role: session.user.role as string,
+        emailVerified: session.user.emailVerified,
+      };
 
-            if (!session) {
-                return res.status(401).json({
-                    success: false,
-                    message: "You are not authorized!"
-                });
-            }
+      if (roles.length && !roles.includes(req.user.role as UserRole)) {
+        return res.status(403).json({
+          success: false,
+          message: "Forbidden! You don't have permission to access this resource!",
+        });
+      }
 
-            req.user = {
-                id: session.user.id,
-                email: session.user.email,
-                name: session.user.name,
-                role: session.user.role as string,
-                emailVerified: session.user.emailVerified
-            };
-
-            if (roles.length && !roles.includes(req.user.role as UserRole)) {
-                return res.status(403).json({
-                    success: false,
-                    message: "Forbidden! You don't have permission to access this resource!"
-                });
-            }
-
-            next();
-        } catch (err) {
-            next(err);
-        }
+      next();
+    } catch (err) {
+      next(err);
     }
+  };
 };
 
 export default auth;
