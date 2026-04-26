d59389e038f3bbd919ef4b1c29cc280754ef33ec
Nurul Alam <nurulalam0404@gmail.com>
2026-04-26 14:17:32 +0600

    fix: fixed tsconfig.json to solve reder deployment issue

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
-import config from "../config"; 
+import config from "../config";
 
 const adapter = new PrismaPg({
   connectionString: config.database_url!,
